f6f86fc33acf35cd40b750c2f2ea688437e60745
Czarny kapsel <uczenczarnykapsel@gmail.com>
2018-12-07 16:58:50 +0100

    add post

--- crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/DataUploadServlet.java
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/DataUploadServlet.java
@@ -2,6 +2,9 @@ package com.infoshareacademy.zajavka.web;
 
 import com.infoshareacademy.zajavka.freemarker.TemplateProvider;
 import freemarker.template.Template;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,15 +15,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import javax.xml.bind.annotation.XmlType;
 import java.io.File;
 import java.io.IOException;
-
-
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/data-upload"})
-
-@MultipartConfig (fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 *5, maxRequestSize = 1024 * 1024 * 5* 5)
-
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+        maxFileSize = 1024 * 1024 * 5,
+        maxRequestSize = 1024 * 1024 * 5 * 5)
 public class DataUploadServlet extends HttpServlet {
 
     @Inject
@@ -29,14 +33,56 @@ public class DataUploadServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(DataUploadServlet.class);
     private static final String TEMPLATE_NAME = "dataUpload";
 
+    private static final String UPLOAD_DIRECTORY = "upload/";
+    private static final int MEMORY_THRESHOLD = 1024 * 1024;
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 5 * 5;
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 5;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) uploadDir.mkdir();
+        try {
+            String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            if (ServletFileUpload.isMultipartContent(req)) {
+                DiskFileItemFactory factory = new DiskFileItemFactory();
+                factory.setSizeThreshold(MEMORY_THRESHOLD);
+                factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+                ServletFileUpload upload = new ServletFileUpload(factory);
+                upload.setFileSizeMax(MAX_FILE_SIZE);
+                upload.setSizeMax(MAX_REQUEST_SIZE);
+
+                List<FileItem> formItems = upload.parseRequest(req);
+                if (formItems != null && formItems.size() > 0) {
+                    for (FileItem item : formItems) {
+                        if (!item.isFormField()) {
+                            String fileName = new File(item.getName()).getName();
+                            String filePath = uploadPath + File.separator + fileName;
+                            File storeFile = new File(filePath);
+                            item.write(storeFile);
+                            req.setAttribute("message", "File "
+                                    + fileName + " has uploaded successfully!");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
     }
+
+
 }
