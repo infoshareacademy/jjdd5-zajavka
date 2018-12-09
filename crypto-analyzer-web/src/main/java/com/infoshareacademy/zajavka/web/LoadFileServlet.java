f0d8759589f12113574b4602b736678f858f514a
Daniel Modrzejewski <danio1118m@gmail.com>
2018-12-09 22:02:36 +0100

    website pattern ready

---
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/LoadFileServlet.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.zajavka.web;
+
+import com.infoshareacademy.zajavka.freemarker.TemplateProvider;
+import com.infoshareacademy.zajavka.service.FileReader;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/load")
+public class LoadFileServlet extends HttpServlet {
+
+    private static final String TEMPLATE_NAME = "loadFile";
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map<String, Object> model = new HashMap<>();
+
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        FileReader fr = new FileReader();
+    }
+}
