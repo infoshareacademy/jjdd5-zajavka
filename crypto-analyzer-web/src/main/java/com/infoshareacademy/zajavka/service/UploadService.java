40d8bb3bb353fc188b700d12747c29ef54f3a7a6
Czarny kapsel <uczenczarnykapsel@gmail.com>
2018-12-10 16:58:48 +0100

    upload only zip

--- crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/service/UploadService.java
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/service/UploadService.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import javax.ejb.Stateless;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
 
 @Stateless
 public class UploadService {
@@ -23,6 +20,7 @@ public class UploadService {
     public static final int MAX_REQUEST_SIZE = 1024 * 1024 * 5 * 5;
     public static final int MAX_FILE_SIZE = 1024 * 1024 * 10;
     public static final String UPLOAD_PATH = System.getProperty("java.io.tmpdir") + "/zajavka-data";
+
 
     public String readFileFromRequest(HttpServletRequest request) {
 
@@ -37,9 +35,9 @@ public class UploadService {
                 LOG.info("Received correct multipart data");
 
                 DiskFileItemFactory factory = new DiskFileItemFactory();
+
                 factory.setSizeThreshold(MEMORY_THRESHOLD);
                 factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-
                 ServletFileUpload upload = new ServletFileUpload(factory);
                 upload.setFileSizeMax(MAX_FILE_SIZE);
                 upload.setSizeMax(MAX_REQUEST_SIZE);
@@ -51,33 +49,26 @@ public class UploadService {
                             String fileName = new File(item.getName()).getName();
                             String filePath = UPLOAD_PATH + File.separator + fileName;
 
-                            LOG.info("Saving file '{}' to '{}'", fileName, filePath);
+                            if (fileName.endsWith(".zip")) {
 
-                            File storeFile = new File(filePath);
-                            item.write(storeFile);
+                                LOG.info("Saving file '{}' to '{}'", fileName, filePath);
 
-                            LOG.info("File saved successfully to '{}'", filePath);
-                            return filePath;
-                            // redirect gdzies gdzie wczytujemy dane z filePath
+                                File storeFile = new File(filePath);
+                                item.write(storeFile);
+
+                                LOG.info("File saved successfully to '{}'", filePath);
+                                return filePath;
+                            }
+                            LOG.warn("Incorect file type");
                         }
                     }
                 }
             }
+
         } catch (Exception e) {
             LOG.error("Error while saving file: {}", e.getMessage());
         }
 
         return null;
     }
-
-//    public class ZipValidator {
-//        public void validate(Path pathToFile) throws IOException {
-//            try {
-//                ZipFile zipFile = new ZipFile(pathToFile.toFile());
-//                String zipname = zipFile.getName();
-//            } catch (ZipException e) {
-//                throw ZipException("Not a zip file", e.getMessage());
-//            }
-//        }
-//    }
 }
