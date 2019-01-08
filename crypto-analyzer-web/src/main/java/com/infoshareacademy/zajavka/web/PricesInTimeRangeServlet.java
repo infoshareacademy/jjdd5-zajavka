4f43a4b090070383e4eff357bd4a7096fd121c1f
BartoszWisniewski <bartosz.wisniewski04@gmail.com>
2019-01-08 21:25:15 +0100

    end configuration part 2

--- crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/PricesInTimeRangeServlet.java
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/PricesInTimeRangeServlet.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
