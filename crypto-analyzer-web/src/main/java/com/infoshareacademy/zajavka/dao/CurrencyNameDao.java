319178dadd2183db8c5ee97ae91475da63aa002b
BartoszWisniewski <bartosz.wisniewski04@gmail.com>
2019-01-10 15:10:57 +0100

    Start

---
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/dao/CurrencyNameDao.java
@@ -0,0 +1,50 @@
+package com.infoshareacademy.zajavka.dao;
+
+import com.infoshareacademy.zajavka.data.Currency;
+import com.infoshareacademy.zajavka.data.CurrencyName;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Stateless
+public class CurrencyNameDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public String save(CurrencyName s) {
+        entityManager.persist(s);
+        return s.getNameFile();
+    }
+
+    public Currency update(Currency s) {
+        return entityManager.merge(s);
+    }
+
+    public void delete(String nameFile) {
+        final CurrencyName s = entityManager.find(CurrencyName.class, nameFile);
+        if (s != null) {
+            entityManager.remove(s);
+        }
+    }
+
+    public Currency findById(String name) {
+        return entityManager.find(Currency.class, name);
+    }
+
+    public List<CurrencyName> findAll() {
+        final Query query = entityManager.createQuery("SELECT s FROM CurrencyName s");
+        return query.getResultList();
+    }
+
+    public Map<String, String> findAllMap() {
+        final Query query = entityManager.createQuery("SELECT s FROM CurrencyName s");
+        List<CurrencyName> currency = query.getResultList();
+        return currency.stream().collect(Collectors.toMap(CurrencyName::getNameFile, CurrencyName::getNameCurrency));
+    }
+
+}
