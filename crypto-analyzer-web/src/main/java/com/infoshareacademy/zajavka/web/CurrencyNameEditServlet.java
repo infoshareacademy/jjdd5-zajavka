07339b5ce3180f4ef75a66dcc3ec7339bf48c476
BartoszWisniewski <bartosz.wisniewski04@gmail.com>
2019-01-10 23:31:53 +0100

    Part 7

--- crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/CurrencyNameEditServlet.java
+++ crypto-analyzer-web/src/main/java/com/infoshareacademy/zajavka/web/CurrencyNameEditServlet.java
@@ -1,14 +1,21 @@
 package com.infoshareacademy.zajavka.web;
 
+import com.infoshareacademy.zajavka.dao.CurrencyNameDao;
+import com.infoshareacademy.zajavka.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/currency-name-edit")
 public class CurrencyNameEditServlet extends HttpServlet {
@@ -16,8 +23,29 @@ public class CurrencyNameEditServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(CurrencyNameEditServlet.class);
     private static final String TEMPLATE_NAME = "currencyNameEdit";
 
+    @Inject
+    private CurrencyNameDao currencyNameDao;
+
+    @Inject
+    private TemplateProvider templateProvider;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+     String value1 = req.getParameter("valueName");
+
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("value", value1);
+
+
+        try {
+            template.process(config, resp.getWriter());
+        } catch (TemplateException e) {
+            LOG.error("Error while processing the template: " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 }
